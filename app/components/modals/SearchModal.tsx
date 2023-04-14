8c52aa16b8eea5d2b58292b179b74162aad9f761
edsattar <ahmed.sattar@live.com>
2023-04-14 18:58:37 +0600

    disable_line_error

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -41,6 +41,8 @@ const SearchModal = () => {
       dynamic(() => import("../Map"), {
         ssr: false,
       }),
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [location]
   );
 
